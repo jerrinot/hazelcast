0aaafdac64e1363a422f4b3567743ed0181815a6
Jaromir Hamala <jaromir.hamala@gmail.com>
2015-01-12 20:59:15 +0000

    another test marked as parallel

--- hazelcast/src/test/java/com/hazelcast/map/MigrationTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/MigrationTest.java
@@ -21,8 +21,10 @@ import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastTestClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
+import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -32,8 +34,8 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
+@RunWith(HazelcastTestClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class MigrationTest extends HazelcastTestSupport {
 
     @Test
