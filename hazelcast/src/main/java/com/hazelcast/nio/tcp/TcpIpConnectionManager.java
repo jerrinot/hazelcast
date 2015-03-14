f37e64974fdba59328abe354a0541d7bc9b4a8f3
Jaromir Hamala <jaromir.hamala@gmail.com>
2015-03-14 21:40:42 +0200

    wip

--- hazelcast/src/main/java/com/hazelcast/nio/tcp/TcpIpConnectionManager.java
+++ hazelcast/src/main/java/com/hazelcast/nio/tcp/TcpIpConnectionManager.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.nio.tcp;
 
+import com.hazelcast.backports.NonBlockingHashMap;
 import com.hazelcast.cluster.impl.BindMessage;
 import com.hazelcast.config.SocketInterceptorConfig;
 import com.hazelcast.logging.ILogger;
@@ -79,7 +80,7 @@ public class TcpIpConnectionManager implements ConnectionManager {
 
     private final boolean socketNoDelay;
 
-    private final Map<Address, Connection> connectionsMap = new ConcurrentHashMap<Address, Connection>(10, 075f, 1);
+    private final ConcurrentMap<Address, Connection> connectionsMap = new NonBlockingHashMap<Address, Connection>();
 
     private final ConcurrentMap<Address, TcpIpConnectionMonitor> monitors =
             new ConcurrentHashMap<Address, TcpIpConnectionMonitor>(100);
@@ -437,7 +438,7 @@ public class TcpIpConnectionManager implements ConnectionManager {
         final Address endPoint = connection.getEndPoint();
         if (endPoint != null) {
             connectionsInProgress.remove(endPoint);
-            connectionsMap.remove(endPoint);
+            connectionsMap.remove(endPoint, connection);
             if (live) {
                 ioService.getEventService().executeEventCallback(new StripedRunnable() {
                     @Override
