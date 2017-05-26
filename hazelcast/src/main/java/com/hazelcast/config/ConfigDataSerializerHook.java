6bfa1990e30ddead81c2fc4b7182acc1e84fdb1c
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-05-26 09:27:23 +0300

    wip

--- hazelcast/src/main/java/com/hazelcast/config/ConfigDataSerializerHook.java
+++ hazelcast/src/main/java/com/hazelcast/config/ConfigDataSerializerHook.java
@@ -16,9 +16,14 @@
 
 package com.hazelcast.config;
 
+import com.hazelcast.internal.dynamicconfig.AddDynamicConfigOperation;
+import com.hazelcast.internal.dynamicconfig.DynamicConfigReplicationOperation;
 import com.hazelcast.internal.serialization.DataSerializerHook;
 import com.hazelcast.internal.serialization.impl.ArrayDataSerializableFactory;
 import com.hazelcast.internal.serialization.impl.FactoryIdHelper;
+import com.hazelcast.map.eviction.LFUEvictionPolicy;
+import com.hazelcast.map.eviction.LRUEvictionPolicy;
+import com.hazelcast.map.eviction.RandomEvictionPolicy;
 import com.hazelcast.nio.serialization.DataSerializableFactory;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.util.ConstructorFunction;
@@ -39,8 +44,26 @@ public final class ConfigDataSerializerHook implements DataSerializerHook {
     public static final int WAN_PUBLISHER_CONFIG = 2;
     public static final int NEAR_CACHE_CONFIG = 3;
     public static final int NEAR_CACHE_PRELOADER_CONFIG = 4;
+    public static final int ADD_DYNAMIC_CONFIG_OP = 5;
+    public static final int REPLICATE_CONFIGURATIONS_OP = 6;
+    public static final int MULTIMAP_CONFIG = 7;
+    public static final int LISTENER_CONFIG = 8;
+    public static final int ENTRY_LISTENER_CONFIG = 9;
+    public static final int MAP_CONFIG = 10;
+    public static final int RANDOM_EVICTION_POLICY = 11;
+    public static final int LFU_EVICTION_POLICY = 12;
+    public static final int LRU_EVICTION_POLICY = 13;
+    public static final int MAP_STORE_CONFIG = 14;
+    public static final int MAP_PARTITION_LOST_LISTENER_CONFIG = 15;
+    public static final int MAP_INDEX_CONFIG = 16;
+    public static final int MAP_ATTRIBUTE_CONFIG = 17;
+    public static final int QUERY_CACHE_CONFIG = 18;
+    public static final int PREDICATE_CONFIG = 19;
+    public static final int PARTITION_STRATEGY_CONFIG = 20;
+    public static final int HOT_RESTART_CONFIG = 21;
 
-    private static final int LEN = NEAR_CACHE_PRELOADER_CONFIG + 1;
+
+    private static final int LEN = HOT_RESTART_CONFIG + 1;
 
     @Override
     public int getFactoryId() {
@@ -74,6 +97,108 @@ public final class ConfigDataSerializerHook implements DataSerializerHook {
         constructors[NEAR_CACHE_PRELOADER_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
             public IdentifiedDataSerializable createNew(Integer arg) {
                 return new NearCachePreloaderConfig();
+            }
+        };
+        constructors[ADD_DYNAMIC_CONFIG_OP] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new AddDynamicConfigOperation();
+            }
+        };
+        constructors[REPLICATE_CONFIGURATIONS_OP] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new DynamicConfigReplicationOperation();
+            }
+        };
+        constructors[MULTIMAP_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new MultiMapConfig();
+            }
+        };
+        constructors[LISTENER_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new ListenerConfig();
+            }
+        };
+        constructors[ENTRY_LISTENER_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new EntryListenerConfig();
+            }
+        };
+        constructors[MAP_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new MapConfig();
+            }
+        };
+        constructors[RANDOM_EVICTION_POLICY] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new RandomEvictionPolicy();
+            }
+        };
+        constructors[LFU_EVICTION_POLICY] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new LFUEvictionPolicy();
+            }
+        };
+        constructors[LRU_EVICTION_POLICY] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new LRUEvictionPolicy();
+            }
+        };
+        constructors[MAP_STORE_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new MapStoreConfig();
+            }
+        };
+        constructors[MAP_PARTITION_LOST_LISTENER_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new MapPartitionLostListenerConfig();
+            }
+        };
+        constructors[MAP_INDEX_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new MapIndexConfig();
+            }
+        };
+        constructors[MAP_ATTRIBUTE_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new MapAttributeConfig();
+            }
+        };
+        constructors[QUERY_CACHE_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new QueryCacheConfig();
+            }
+        };
+        constructors[PREDICATE_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new PredicateConfig();
+            }
+        };
+        constructors[PARTITION_STRATEGY_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new PartitioningStrategyConfig();
+            }
+        };
+        constructors[HOT_RESTART_CONFIG] = new ConstructorFunction<Integer, IdentifiedDataSerializable>() {
+            @Override
+            public IdentifiedDataSerializable createNew(Integer arg) {
+                return new HotRestartConfig();
             }
         };
 
